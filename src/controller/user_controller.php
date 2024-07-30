e0b7077498a65bfe5f8288dc9f9de4493f5d0181
Azzam Syakir <azzamsykir@gmail.com>
2024-07-30 11:07:52 +0700

    feat: add transaction in register

--- src/controller/user_controller.php
+++ src/controller/user_controller.php
@@ -1,6 +1,8 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 require_once __DIR__ . '/../db.php';
-
+require 'vendor/autoload.php';
 class UserController
 {
     public function get()
@@ -44,12 +46,14 @@ class UserController
     public function Register()
     {
         $conn = ConnectDB();
+        $conn->begin_transaction();
 
         $input = !empty($_POST) ? $_POST : json_decode(file_get_contents('php://input'), true);
         $username = isset($input['username']) ? $input['username'] : null;
         $password = isset($input['password']) ? $input['password'] : null;
         $email = isset($input['email']) ? $input['email'] : null;
         $password_hash = password_hash($password, PASSWORD_DEFAULT);
+        $token = password_hash(bin2hex(random_bytes(16)), PASSWORD_DEFAULT);
         header('Content-Type: application/json');
         if (empty($username)) {
             echo json_encode(['status' => 'error', 'message' => 'Name must be filled'], JSON_PRETTY_PRINT);
@@ -59,14 +63,45 @@ class UserController
             try {
                 $id = substr(sha1(time()), 0, 10);
     
-                $query = "INSERT INTO users (id, username, password, email, confirmed) VALUES ('$id', '$username', '$password_hash', '$email', false)";
+                $query = "INSERT INTO users (id, username, password, email, token ,confirmed) VALUES ('$id', '$username', '$password_hash', '$email', '$token', false)";
                 $result = $conn->query($query);
-    
-                if ($result) {
-                    $conn->commit();
-                    echo json_encode(['status' => 'success', 'message'=> 'Register Success'], JSON_PRETTY_PRINT);
-                } else {
-                    throw new Exception($conn->error);
+                // send email
+                $mail = new PHPMailer(true);
+                $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+                $dotenv->load();
+                $appHost = $_ENV['APP_HOST'];
+                $senderEmailAddress = $_ENV['SENDER_EMAIL_ADDRESS'];
+                $senderEmailPassword = $_ENV['SENDER_EMAIL_PASSWORD'];
+                $activation_link = $appHost . "/confirm/" . $token ;
+                // Server settings
+                $mail->isSMTP(); 
+                $mail->Host = "smtp.gmail.com"; 
+                $mail->SMTPAuth   = true; 
+                $mail->Username = $senderEmailAddress; 
+                $mail->Password = $senderEmailPassword;
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;           
+                $mail->Port       = 465;
+                // recipients
+                $mail->setFrom($senderEmailAddress, 'activation-todo');
+                $mail->AddAddress($email);
+                $mail->Subject='Please activate your account';;
+                $mail->Body     = <<<MESSAGE
+                        Hi,
+                        Please click the following link to activate your account:
+                        $activation_link
+                        MESSAGE;;
+                $mail->WordWrap = 200;
+                $mail->send();
+                if(!$mail->Send()) {
+                    echo json_encode(['status' => 'error', 'message' => "send email failed: " . $mail->ErrorInfo], JSON_PRETTY_PRINT);
+                }
+                else {
+                    if ($result) {
+                        $conn->commit();
+                        echo json_encode(['status' => 'success', 'message'=> 'Register Success'], JSON_PRETTY_PRINT);
+                    } else {
+                        throw new Exception($conn->error);
+                    }
                 }
             } catch (Exception $e) {
                 $conn->rollback();
